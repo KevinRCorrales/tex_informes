64253a58fd5b119a2b18b9bae3e4d70cfe9a5f1e
KevinRCorrales <kevin.ramirezcorr@campusucc.edu.co>
2026-05-01 19:19:54 -0500

    primer commit del momento 3

---
+++ automatas/momento3/sketch_grupo1_m3/sketch_grupo1_m3.ino
@@ -0,0 +1,29 @@
+#include <LiquidCrystal_I2C.h>  // Librería necesaria para manejar el LCD I2C
+#include <Servo.h>              // Librería necesaria para manejar el servomotor
+#include <IRremote.hpp>         // Librería necesaria para manejar el receptor infrarrojo y el control remoto
+#include <Keypad.h>             // Librería necesaria para el manejo del keypad 4x4
+
+// Variables de Estado y Pines a usar en el montaje
+const byte SERVO_PIN = 10;
+const byte PIN_RECEPTOR = 11;
+const byte PIN_LED = 13;
+const byte BUZZER = 12;
+const byte PIN_LED_EXITO = A1;  // No hay mas pines :(
+
+byte rowPins[ROWS] = { 9, 8, 7, 6 };  // Pines a usar para las filas
+byte colPins[COLS] = { 5, 4, 3, 2 };  // Pines a usar para las columnas
+
+/*
+============================================
+Funciones para debug en serial ok funciona para cuando algo se inicia correctamente
+y fail para cuando se producen fallos y el usuario debe conocerlos
+*/
+void ok(const __FlashStringHelper* msg) {
+  Serial.print(F("[  OK  ] "));
+  Serial.println(msg);
+}
+
+void fail(const __FlashStringHelper* msg) {
+  Serial.print(F("[FAILED] "));
+  Serial.println(msg);
+}
